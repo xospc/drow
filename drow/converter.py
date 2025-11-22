1abd640546118d3d660ffabf15b0b5ae206ac2d4
Elephant Liu <lexdene@users.noreply.github.com>
2025-11-22 11:33:11 +0800

    refactor: remove too simple converters

--- drow/converter.py
+++ drow/converter.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from typing import TypeVar
 from collections.abc import Callable
 
@@ -8,11 +7,3 @@ Converter = Callable[[str], T]
 
 def no_op(value: T) -> T:
     return value
-
-
-def convert_to_float(value: str) -> float:
-    return float(value)
-
-
-def convert_to_decimal(value: str) -> Decimal:
-    return Decimal(value)
